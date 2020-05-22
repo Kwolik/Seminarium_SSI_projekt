a1a724875480b0257760c7c36a5d01d74c7cc84b
Kwolik <kwolikzsp6@gmail.com>
2020-05-22 20:30:04 +0200

    projekt
    projekcik

---
+++ paint_test/paint_test/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace paint_test
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void paint_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button.Name == "pen")
+                MyCanvas.EditingMode = InkCanvasEditingMode.Ink;
+
+            if (button.Name == "select")
+                MyCanvas.EditingMode = InkCanvasEditingMode.Select;
+
+            if (button.Name == "erased")
+                MyCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if(button.Name == "save")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "InkCanvas Format|*.jpg";
+                sfd.Title = "Save InkCanvas File";
+                sfd.ShowDialog();
+                if (sfd.FileName == "") return;
+                FileStream fs = File.Open(sfd.FileName, FileMode.CreateNew);
+                MyCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+
+            if (button.Name == "porownaj")
+            {
+                Bitmap bitmap = DrawControlToBitmap(MyCanvas);
+                bitmap.Save("siemanko.jpg");
+                System.Diagnostics.Process.Start("siemanko.jpg");
+            }
+                
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = new FileStream("MyPicture.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection sc = new StrokeCollection(fs);
+                this.MyCanvas.Strokes = sc;
+            }
+        }
+
+        private void porownaj_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            SaveButton_Click(button, e);
+            PunktyKluczowe.punktyKluczowe();
+            
+        }
+
+        private static Bitmap DrawControlToBitmap(Control control)
+        {
+            Bitmap bitmap = new Bitmap(control.Width, control.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            Rectangle rect = control.RectangleToScreen(control.ClientRectangle);
+            graphics.CopyFromScreen(rect.Location, Point.Empty, control.Size);
+            return bitmap;
+        }
+    }
+}
---
+++ paint_test/paint_test/PunktyKluczowe.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace paint_test
+{
+    class PunktyKluczowe
+    {
+        public static void punktyKluczowe()
+        {
+            double K = 0;
+            Bitmap btmz = new Bitmap(@"siema.jpg");
+            Bitmap btmF = new Bitmap(btmz.Width, btmz.Height);
+
+            double[][] kernel = new double[3][];
+            kernel[0] = new double[] { -1, -1, -1 };
+            kernel[1] = new double[] { -1, 8, -1 };
+            kernel[2] = new double[] { -1, -1, -1 };
+
+            for (int i = 0; i < kernel.Length; i++)
+            {
+                for (int j = 0; j < kernel.Length; j++)
+                {
+                    K += kernel[i][j];
+                }
+            }
+
+            if (K == 0)
+                K = 1;
+
+            for (int i = 0; i < btmz.Width - kernel.Length; i++)
+            {
+                for (int j = 0; j < btmz.Height - kernel[0].Length; j++)
+                {
+                    double convR = 0, convG = 0, convB = 0;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 3; l++)
+                        {
+                            convR += 1 / K * kernel[k][l] * btmz.GetPixel(i - k + kernel.Length, j - l + kernel.Length).R;
+                            convG += 1 / K * kernel[k][l] * btmz.GetPixel(i - k + kernel.Length, j - l + kernel.Length).G;
+                            convB += 1 / K * kernel[k][l] * btmz.GetPixel(i - k + kernel.Length, j - l + kernel.Length).B;
+                        }
+
+                        if (convR < 0) convR = 0;
+                        if (convG < 0) convG = 0;
+                        if (convB < 0) convB = 0;
+                    }
+                    if (convR > 255) convR = 255;
+                    if (convG > 255) convG = 255;
+                    if (convB > 255) convB = 255;
+
+                    Color c = Color.FromArgb((int)convR, (int)convG, (int)convB);
+
+                    if (c.GetBrightness() == 0)          //Sprawdza czy dany punkt jest tym najciemniejszym
+                        btmF.SetPixel(i, j, Color.FromArgb(240, 0, 0)); //jesli tak to go zamalowuje na czerwono (tutaj trzeba potem zmienic)
+                    else
+                        btmF.SetPixel(i, j, Color.FromArgb(btmz.GetPixel(i, j).R, btmz.GetPixel(i, j).G, btmz.GetPixel(i, j).B));
+
+                }
+            }
+            btmF.Save(@"essa2.jpg");
+        }
+    }
+}
