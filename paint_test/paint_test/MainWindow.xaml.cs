64859bbb9157ca2c98b983c9169f7f5da819df36
Kwolik <kwolikzsp6@gmail.com>
2020-05-27 01:12:39 +0200

    porownywanie poprawic
    ok

--- paint_test/paint_test/MainWindow.xaml.cs
+++ paint_test/paint_test/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Rectangle = System.Drawing.Rectangle;
+using Microsoft.VisualBasic;
 
 namespace paint_test
 {
@@ -28,6 +29,8 @@ namespace paint_test
         public MainWindow()
         {
             InitializeComponent();
+            MyCanvas.DefaultDrawingAttributes.Width = 32;
+            MyCanvas.DefaultDrawingAttributes.Height = 32;
         }
 
         private void paint_Click(object sender, RoutedEventArgs e)
@@ -44,56 +47,96 @@ namespace paint_test
                 MyCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
         }
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        private void porownaj_Click(object sender, RoutedEventArgs e)
         {
-            Button button = (Button)sender;
+            zapiszRysunek("savedimage");
 
-            if (button.Name == "save")
+            PunktyKluczowe pkt = new PunktyKluczowe();
+            IDictionary<string, double> dict = new Dictionary<string, double>();
+
+            foreach (string fileName in Directory.GetFiles(@"litery\"))
             {
-                FileStream fs = File.Open("save.bin", FileMode.CreateNew);
-                MyCanvas.Strokes.Save(fs);
+                dict.Add(fileName, pkt.porownajZBaza(fileName));
+            }
+
+            var sortedDict = from entry in dict orderby entry.Value ascending select entry;
+            dict = sortedDict.OrderByDescending(i => i.Value).ToDictionary(i => i.Key, i => i.Value);
+
+            textBlock1.Text = dict.Keys.ElementAt(0).Substring(7, 1);
+            textBlock2.Text = dict.Keys.ElementAt(1).Substring(7, 1);
+            textBlock3.Text = dict.Keys.ElementAt(2).Substring(7, 1);
+
+            textBlockPlik1.Text = dict.Keys.ElementAt(0).Substring(7);
+            textBlockPlik2.Text = dict.Keys.ElementAt(1).Substring(7);
+            textBlockPlik3.Text = dict.Keys.ElementAt(2).Substring(7);
+
+            textBlockProcent1.Text = dict.Values.ElementAt(0).ToString() + " % ";
+            textBlockProcent2.Text = dict.Values.ElementAt(1).ToString() + " % ";
+            textBlockProcent3.Text = dict.Values.ElementAt(2).ToString() + " % ";
+
+        }
+
+
+        private void zapiszRysunek(String nazwaPliku)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "savedimage.jpg"; 
+            int krotnoscPliku = 0;
+            string filename;
+
+            if (nazwaPliku != "savedimage") 
+            {
+                if (File.Exists(@"litery\" + nazwaPliku + ".jpg"))
+                    do
+                    {
+                        krotnoscPliku++;
+                    } while (File.Exists(@"litery\" + nazwaPliku + krotnoscPliku + ".jpg")); // sprawdza czy plik o danej nazwie istnieje
+
+                filename = @"litery\" +  nazwaPliku + krotnoscPliku + ".jpg";
+            }
+            else
+                filename = dlg.FileName;
+
+
+
+            int width = (int)this.MyCanvas.ActualWidth;
+            int height = (int)this.MyCanvas.ActualHeight;
+            RenderTargetBitmap rtb =
+            new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(MyCanvas);
+            
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                File.SetAttributes(filename, FileAttributes.Normal);
+                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(rtb));
+                encoder.Save(fs);
                 fs.Close();
             }
 
-            if (button.Name == "porownaj")
-            {
-                // Save document
-                string filename = "savedimage.jpg";
-                //get the dimensions of the ink control
-                int margin = (int)this.MyCanvas.Margin.Left;
-                int width = (int)this.MyCanvas.ActualWidth - margin;
-                int height = (int)this.MyCanvas.ActualHeight - margin;
-                //render ink to bitmap
-                RenderTargetBitmap rtb =
-                new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
-                rtb.Render(MyCanvas);
-
-                using (FileStream fs = new FileStream(filename, FileMode.Create))
-                {
-                    BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-                    encoder.Frames.Add(BitmapFrame.Create(rtb));
-                    encoder.Save(fs);
-                }
-
-            }
-                
         }
 
-        private void OpenButton_Click(object sender, RoutedEventArgs e)
+
+        private void zapisz_Click(object sender, RoutedEventArgs e)
         {
-            using (FileStream fs = new FileStream("save.bin", FileMode.Open, FileAccess.Read))
-            {
-                StrokeCollection sc = new StrokeCollection(fs);
-                this.MyCanvas.Strokes = sc;
-            }
+            InputBox.Visibility = System.Windows.Visibility.Visible;
         }
 
-        private void porownaj_Click(object sender, RoutedEventArgs e)
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
         {
-            Button button = (Button)sender;
-            SaveButton_Click(button, e);
-            PunktyKluczowe.punktyKluczowe();
-            
+            InputBox.Visibility = System.Windows.Visibility.Collapsed;
+            String input = InputTextBox.Text;
+            zapiszRysunek(input);
+            InputTextBox.Text = String.Empty;
         }
+
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputBox.Visibility = System.Windows.Visibility.Collapsed;
+            InputTextBox.Text = String.Empty;
+        }
+
     }
 }
--- paint_test/paint_test/PunktyKluczowe.cs
+++ paint_test/paint_test/PunktyKluczowe.cs
@@ -4,65 +4,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Controls;
+using System.IO;
+using System.Threading;
 
 namespace paint_test
 {
-    class PunktyKluczowe
+    class PunktyKluczowe : MainWindow
     {
-        public static void punktyKluczowe()
+        public PunktyKluczowe()
         {
-            double K = 0;
-            Bitmap btmz = new Bitmap(@"savedimage.jpg");
-            Bitmap btmF = new Bitmap(btmz.Width, btmz.Height);
-
-            double[][] kernel = new double[3][];
-            kernel[0] = new double[] { -1, -1, -1 };
-            kernel[1] = new double[] { -1, 8, -1 };
-            kernel[2] = new double[] { -1, -1, -1 };
-
-            for (int i = 0; i < kernel.Length; i++)
-            {
-                for (int j = 0; j < kernel.Length; j++)
-                {
-                    K += kernel[i][j];
-                }
-            }
-
-            if (K == 0)
-                K = 1;
-
-            for (int i = 0; i < btmz.Width - kernel.Length; i++)
-            {
-                for (int j = 0; j < btmz.Height - kernel[0].Length; j++)
-                {
-                    double convR = 0, convG = 0, convB = 0;
-                    for (int k = 0; k < 3; k++)
-                    {
-                        for (int l = 0; l < 3; l++)
-                        {
-                            convR += 1 / K * kernel[k][l] * btmz.GetPixel(i - k + kernel.Length, j - l + kernel.Length).R;
-                            convG += 1 / K * kernel[k][l] * btmz.GetPixel(i - k + kernel.Length, j - l + kernel.Length).G;
-                            convB += 1 / K * kernel[k][l] * btmz.GetPixel(i - k + kernel.Length, j - l + kernel.Length).B;
-                        }
-
-                        if (convR < 0) convR = 0;
-                        if (convG < 0) convG = 0;
-                        if (convB < 0) convB = 0;
-                    }
-                    if (convR > 255) convR = 255;
-                    if (convG > 255) convG = 255;
-                    if (convB > 255) convB = 255;
-
-                    Color c = Color.FromArgb((int)convR, (int)convG, (int)convB);
-
-                    if (c.GetBrightness() == 0)          //Sprawdza czy dany punkt jest tym najciemniejszym
-                        btmF.SetPixel(i, j, Color.FromArgb(240, 0, 0)); //jesli tak to go zamalowuje na czerwono (tutaj trzeba potem zmienic)
-                    else
-                        btmF.SetPixel(i, j, Color.FromArgb(btmz.GetPixel(i, j).R, btmz.GetPixel(i, j).G, btmz.GetPixel(i, j).B));
-
-                }
-            }
-            btmF.Save(@"essa2.jpg");
         }
+
+        public double porownajZBaza(string nazwaPlikuZBazy)
+        {
+
+            using (Bitmap btm = new Bitmap(@"savedImage.jpg"))
+            {
+
+                Bitmap btmF = new Bitmap(nazwaPlikuZBazy);
+                Bitmap btmTest = new Bitmap(btm.Width, btm.Height);
+
+                double iloscCzarnych = 0;
+                double iloscPrzeciwnych = 0;
+                double sumaCzarnych = 0;
+                double sumaCzarnychOryginal = 0;
+                double wynik = 0;
+
+                for (int Xcount = 0; Xcount < btm.Width; Xcount++)
+                {
+                    for (int Ycount = 0; Ycount < btm.Height; Ycount++)
+                    {
+
+                        if (((btm.GetPixel(Xcount, Ycount).R == 0) && (btm.GetPixel(Xcount, Ycount).G == 0) && (btm.GetPixel(Xcount, Ycount).B == 0)) && ((btmF.GetPixel(Xcount, Ycount).R == 255) && (btmF.GetPixel(Xcount, Ycount).G == 255) && (btmF.GetPixel(Xcount, Ycount).B == 255)))
+                            iloscPrzeciwnych++; //narysowany czarny, z bazy bialy
+
+
+                        if (((btm.GetPixel(Xcount, Ycount).R == 0) && (btm.GetPixel(Xcount, Ycount).G == 0) && (btm.GetPixel(Xcount, Ycount).B == 0)) && ((btmF.GetPixel(Xcount, Ycount).R == 0) && (btmF.GetPixel(Xcount, Ycount).G == 0) && (btmF.GetPixel(Xcount, Ycount).B == 0)))
+                            iloscCzarnych++; // narysowany czarny, z bazy czarny
+
+
+                        if ((btm.GetPixel(Xcount, Ycount).R == 0) && (btm.GetPixel(Xcount, Ycount).G == 0) && (btm.GetPixel(Xcount, Ycount).B == 0))
+                            sumaCzarnych++;
+
+                        if ((btmF.GetPixel(Xcount, Ycount).R == 0) && (btmF.GetPixel(Xcount, Ycount).G == 0) && (btmF.GetPixel(Xcount, Ycount).B == 0))
+                            sumaCzarnychOryginal++;
+                    }
+                }
+
+
+                if (sumaCzarnych >= sumaCzarnychOryginal * 0.95)
+                {
+                    //iloscCzarnych zróżnicowanie
+                    if (iloscCzarnych >= sumaCzarnych * 0.8) iloscCzarnych += iloscCzarnych * 0.05;
+                    else if (iloscCzarnych < sumaCzarnych * 0.8 && iloscCzarnych > sumaCzarnych * 0.75) iloscCzarnych += iloscCzarnych * 0.08;
+                    else if (iloscCzarnych <= sumaCzarnych * 0.75 && iloscCzarnych > sumaCzarnych * 0.7) iloscCzarnych += iloscCzarnych * 0.12;
+                    else if (iloscCzarnych <= sumaCzarnych * 0.7 && iloscCzarnych > sumaCzarnych * 0.65) iloscCzarnych += iloscCzarnych * 0.15;
+                    else if (iloscCzarnych <= sumaCzarnych * 0.65 && iloscCzarnych > sumaCzarnych * 0.6) iloscCzarnych += iloscCzarnych * 0.18;
+                    else if (iloscCzarnych <= sumaCzarnych * 0.6 && iloscCzarnych > sumaCzarnych * 0.55) iloscCzarnych += iloscCzarnych * 0.2;
+                    else iloscCzarnych += iloscCzarnych * 0.25;
+
+                    //iloscPrzeciwnych zróżnicowanie
+                    if (iloscPrzeciwnych <= sumaCzarnych * 0.2) iloscPrzeciwnych /= 10 + iloscPrzeciwnych * 0.1;
+                    else if (iloscPrzeciwnych > sumaCzarnych * 0.2 && iloscPrzeciwnych <= sumaCzarnych * 0.25) iloscPrzeciwnych /= 9 + iloscPrzeciwnych * 0.12;
+                    else if (iloscPrzeciwnych > sumaCzarnych * 0.25 && iloscPrzeciwnych <= sumaCzarnych * 0.3) iloscPrzeciwnych /= 8 + iloscPrzeciwnych * 0.14;
+                    else if (iloscPrzeciwnych > sumaCzarnych * 0.3 && iloscPrzeciwnych <= sumaCzarnych * 0.35) iloscPrzeciwnych /= 7 + iloscPrzeciwnych * 0.16;
+                    else if (iloscPrzeciwnych > sumaCzarnych * 0.35 && iloscPrzeciwnych <= sumaCzarnych * 0.4) iloscPrzeciwnych /= 6 + iloscPrzeciwnych * 0.18;
+                    else if (iloscPrzeciwnych > sumaCzarnych * 0.4 && iloscPrzeciwnych <= sumaCzarnych * 0.45) iloscPrzeciwnych /= 5 + iloscPrzeciwnych * 0.2;
+                    else iloscPrzeciwnych /= 4 + iloscPrzeciwnych * 0.2;
+
+                    wynik = (iloscCzarnych - iloscPrzeciwnych) / sumaCzarnych;
+                }
+
+                else
+                    wynik = 0;
+
+                if (sumaCzarnych < 6000)
+                    wynik = 0;
+
+            }
+        }
+
     }
 }
